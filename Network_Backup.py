23bc6f6e990e28c33458c34c9a51c883b81e2972
souljer4-github <spamroyals7@gmail.com>
2022-03-12 17:29:58 -0800

    modified network backup and removed csrbackup file

--- Network_Backup.py
+++ Network_Backup.py
@@ -1,11 +1,7 @@
-import datetime
-import pathlib
-import os
-import netmiko
-import getpass
 from getpass import getpass
 from pathlib import Path
 from netmiko import ConnectHandler
+from netmiko.ssh_exception import AuthenticationException, SSHException, NetmikoTimeoutException
 
 u = input("Please provide a username: ")
 p = getpass(prompt='Enter your password: ')
@@ -14,10 +10,23 @@ f = 'csrBackup.txt'
 w = 'w'
 file = open(f, w)
 ip = '192.168.253.11'
-dict1 = {'ip': ip, 'username': u, 'password': p, 'device_type': 'cisco_ios'}
-connection = ConnectHandler(**dict1)
-output=connection.send_command('show run')
-output2=str(datetime.datetime.now())
-file.write(output)
-file.write(output2)
+ciscoDevice = {'ip': ip, 'username': u, 'password': p, 'device_type': 'cisco_ios'}
+try:
+    connection = ConnectHandler(**ciscoDevice)
+except (AuthenticationException):
+    print('Authentication failed; connection refused on following device: ' + ciscoDevice['ip'])
+except (NetmikoTimeoutException):
+    print('The following device has timed out: ' + ciscoDevice['ip'])
+except (SSHException):
+    print('The device was unable to connect using SSH. Please check your configuration setting on: ' + ciscoDevice['ip'])
+except Exception as other_error:
+    print('The error' + str(other_error) + 'occurred while attempting to connect to: ' + ciscoDevice['ip'])
+
+output_1 = connection.send_command('show run')
+output_2 = connection.send_command('show ip int brief')
+print(output_1)
+print(output_2)
+#output2=str(datetime.datetime.now())
+file.write(output_1)
+#file.write(output2)
 file.close()
